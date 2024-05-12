77512711c090a3d2e4bc408f6f653c3461dd6615
VioletEvergardenZz <1561862432@qq.com>
2024-05-12 16:05:36 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <!-- html -->
+    <Person/>
+
+</template>
+
+<script>
+    // JS或TS
+    import Person from './components/Person.vue'
+    export default{
+        name:'App', //组件名
+        components:{Person}  //注册组件
+    }
+</script>
+
+
---
+++ src/components/Person.vue
@@ -0,0 +1,44 @@
+<template>
+    <!-- html -->
+    <div class="person">
+        <h2>姓名 {{name}}</h2>
+        <h2>年龄 {{age}}</h2>
+        <h2>地址 {{address}}</h2>
+        <button @click="changeName">修改名字</button>
+        <button @click="changeAge">修改年龄</button>
+        <button @click="showTel">查看联系事件</button>
+    </div>
+</template>
+
+<script setup lang="ts" name="Person234">
+    // 数据 不是响应式
+    let name='xiaoxiao'
+    let age=18
+    let tel=13888888888
+    let address='嘉兴市'
+
+    // 方法
+    function changeName(){
+        name='xiaoxiao520'  //修改name 页面无变化
+        console.log(name) 
+    }
+    function changeAge() {
+        age+=1
+    }
+    function showTel() {
+        alert(tel)
+    }
+</script>
+
+<style scoped>
+    /* 样式 */
+    .person{
+        background-color: skyblue;
+        box-shadow: 0 0 10px;
+        border-radius: 10px;
+        padding: 20px;
+    }
+    button{
+        margin: 0 5px;
+    }
+</style>
